33777fdbc917be69bdb3d1908cf670bae9bd4a07
Stan Borbat [@sborbat] <stan@borbat.com>
2019-02-11 09:48:15 -0500

    Disabling the statement timeout

--- src/UnloadCopyUtility/util/redshift_cluster.py
+++ src/UnloadCopyUtility/util/redshift_cluster.py
@@ -12,8 +12,8 @@ from pg import connect
 
 options = "keepalives=1 keepalives_idle=200 keepalives_interval=200 keepalives_count=6 connect_timeout=10"
 
-set_timeout_stmt = "set statement_timeout = 604800000" # One week
-
+#set_timeout_stmt = "set statement_timeout = 604800000" # One week
+set_timeout_stmt = "set statement_timeout = 0" # Unlimited
 
 class RedshiftClusterFactory:
     def __init__(self):
